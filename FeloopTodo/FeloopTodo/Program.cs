41bc8d8ffbd5351eb8e3f404377c982249619a9f
BuildTools <unconfigured@null.spigotmc.org>
2017-04-26 21:54:23 +0300

    menü yaratıldı.

--- FeloopTodo/FeloopTodo/Program.cs
+++ FeloopTodo/FeloopTodo/Program.cs
@@ -6,7 +6,15 @@ namespace FeloopTodo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Konsolda unicode karakterlerin gözükmesi için gerekli, yoksa Türkçe karakterler gözükmeyecek.
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("###########################################");
+            Console.WriteLine("# 1- Yeni bir görev tanımla.              #");
+            Console.WriteLine("# 2- Görevi tamamlanmış olarak işaretle.  #");
+            Console.WriteLine("# 3- Görev sil.                           #");
+            Console.WriteLine("###########################################");
+            Console.ReadKey();
         }
     }
 }
