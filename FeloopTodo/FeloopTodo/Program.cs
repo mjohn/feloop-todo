23144f6474aa35768247cfd7ac355c2f58cc0e75
BuildTools <unconfigured@null.spigotmc.org>
2017-04-27 20:02:57 +0300

    menü seçimlerininde seçilen işleme göre ekrana hangi seçimin yapıldığını gösteren yazılar eklendi.

--- FeloopTodo/FeloopTodo/Program.cs
+++ FeloopTodo/FeloopTodo/Program.cs
@@ -6,15 +6,36 @@ namespace FeloopTodo
     {
         static void Main(string[] args)
         {
+
+
             // Konsolda unicode karakterlerin gözükmesi için gerekli, yoksa Türkçe karakterler gözükmeyecek.
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
             Console.WriteLine("###########################################");
             Console.WriteLine("# 1- Yeni bir görev tanımla.              #");
             Console.WriteLine("# 2- Görevi tamamlanmış olarak işaretle.  #");
-            Console.WriteLine("# 3- Görev sil.                           #");
+            Console.WriteLine("# 9- Görev sil.                           #");
             Console.WriteLine("###########################################");
-            Console.ReadKey();
+            while (true)
+            {
+
+                var key = Console.ReadKey();
+                Console.Write(Environment.NewLine);
+                Console.WriteLine("Lütfen seçim yapınız: ");
+
+                if (key.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine("1 tuşuna basıldı.");
+                }
+                else if (key.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine("2 tuşuna basıldı.");
+                }
+                else if (key.Key == ConsoleKey.D9)
+                {
+                    Console.WriteLine("9 tuşuna basıldı.");
+                }
+            }
         }
     }
 }
