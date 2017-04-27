503a2cef08f21aaf2f37903f8b38b7f68799cbce
BuildTools <unconfigured@null.spigotmc.org>
2017-04-27 22:19:18 +0300

    task class'ı oluşturuldu

---
+++ FeloopTodo/FeloopTodo/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeloopTodo
+{
+    public class Task
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
