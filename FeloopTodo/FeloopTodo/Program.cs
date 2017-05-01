b27a9cdf8df7a90f5074749f599fe5443e7fa07d
mjohn <mehmetcankamar@gmail.com>
2017-05-01 15:13:21 +0300

    tamamlandi.

--- FeloopTodo/FeloopTodo/Program.cs
+++ FeloopTodo/FeloopTodo/Program.cs
@@ -8,39 +8,51 @@ namespace FeloopTodo
         public static List<Task> TaskList { get; set; }
         static void Main(string[] args)
         {
-
+            TaskList = new List<Task>();
 
             // Konsolda unicode karakterlerin gözükmesi için gerekli, yoksa Türkçe karakterler gözükmeyecek.
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-            Console.WriteLine("###########################################");
-            Console.WriteLine("# 1- Yeni bir görev tanımla.              #");
-            Console.WriteLine("# 2- Görevi tamamlanmış olarak işaretle.  #");
-            Console.WriteLine("# 3- Görevleri listele.                   #");
-            Console.WriteLine("# 9- Görev sil.                           #");
-            Console.WriteLine("###########################################");
             while (true)
             {
+                Console.WriteLine("###########################################");
+                Console.WriteLine("# 1- Yeni bir görev tanımla.              #");
+                Console.WriteLine("# 2- Görevi tamamlanmış olarak işaretle.  #");
+                Console.WriteLine("# 3- Görevleri listele.                   #");
+                Console.WriteLine("# 9- Görev sil.                           #");
+                Console.WriteLine("###########################################");
 
+                Console.WriteLine("Lütfen seçim yapınız: ");
                 var key = Console.ReadKey();
                 Console.Write(Environment.NewLine);
-                Console.WriteLine("Lütfen seçim yapınız: ");
+                
 
                 if (key.Key == ConsoleKey.D1)
                 {
-                    Console.WriteLine("1 tuşuna basıldı.");
+                    Task task = new Task();
+                    task.CreatedAt = DateTime.Now;
+                    task.IsCompleted = false;
+                    Console.WriteLine("Görev açklaması giriniz: ");
+                    task.Description = Console.ReadLine();
+                    AddTask(task);
                 }
                 else if (key.Key == ConsoleKey.D2)
                 {
-                    Console.WriteLine("2 tuşuna basıldı.");
+                    ListTasks();
+                    Console.WriteLine("Tamamlandı olarak işaretlenecek görevin id'sini yazınız: ");
+                    int index = Convert.ToInt32(Console.ReadLine());
+                    CompleteTask(index);
                 }
                 else if (key.Key == ConsoleKey.D3)
                 {
-                    Console.WriteLine("3 tuşuna basıldı.");
+                    ListTasks();
                 }
                 else if (key.Key == ConsoleKey.D9)
                 {
-                    Console.WriteLine("9 tuşuna basıldı.");
+                    ListTasks();
+                    Console.WriteLine("Silinecek görevin id'sini yazınız: ");
+                    int index = Convert.ToInt32(Console.ReadLine());
+                    DeleteTask(index);
                 }
             }
         }
